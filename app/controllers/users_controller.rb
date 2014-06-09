30c9c177f71cb71b4f55ca8b4f0f5c1d8a0352c7
Nathan Tempelman <nathantempelman@gmail.com>
2014-06-09 11:49:15 -0400

    Tried to fix the forms to use bootstrap, it's a lost cause. Just going to home style it, fuck bootstrap 3+rails. Just in case I want to try again, I'm going to keep this code here.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,6 +67,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password,
+                                                  :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ActiveRecord::Base
 							uniqueness: true
 	#validates :password, presence: true, length: {minimum: 6, maximum: 22}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
-  	validates :email, :allow_blank => true, format: { with: VALID_EMAIL_REGEX }
-  	has_secure_password
-  	validates :password, length: {minimum: 6}
+	validates :email, :allow_blank => true, format: { with: VALID_EMAIL_REGEX }
+	has_secure_password
+	validates :password, length: {minimum: 6}
+#  validates_confirmation_of :password
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
