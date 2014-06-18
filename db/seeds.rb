413b288ee94af8b815cc8b0b76a4ca5dab08112a
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-06-18 10:02:42 -0400

    Database Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'net/http'
+require 'json'
+
+count = 0
+for page in 0..5 # 50 is the highest we can put the page here.
+  puts "\n Page ##{page.to_s}"
+  json = JSON.parse Net::HTTP.get('imgur.com', "/r/cats.json/page/#{page}")
+  json['data'].each do |image_json|
+    url = "http://imgur.com/#{image_json['hash']}#{image_json['ext']}"
+    picture = Picture.create(url: url, name: image_json['title'], rating: 1500)
+    puts "~> Created image named #{image_json['title']} with URL #{url}"
+    count += 1
+  end
+end
+puts "\n\nTried to create #{count.to_s} pictures!\nSucceeded with #{Picture.count} pictures."
